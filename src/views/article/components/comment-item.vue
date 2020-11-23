28bfbd683789a99275bf515bf5c33b3ee2ba6cb4
likai <1774574746@qq.com>
2020-11-23 16:31:58 +0800

    评论功能进阶

---
+++ src/views/article/components/comment-item.vue
@@ -0,0 +1,34 @@
+<template>
+  <van-cell
+    class="comment-item"
+  >
+    <van-image
+      slot="icon"
+      width="35"
+      height="35"
+      round
+      fixed
+      :src='comment.aut_photo'
+    />
+    <div slot="title">
+      <div>{{ comment.aut_name }}</div>
+      <div>{{ comment.content }}</div>
+    </div>
+  </van-cell>
+</template>
+
+<script>
+export default {
+  name: 'CommentItem',
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/article/components/comment-list.vue
+++ src/views/article/components/comment-list.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="comment-list">
+    <van-cell title="全部评论"/>
     <van-list
       v-model="loading"
       :finished="finished"
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <!-- <van-cell v-for="(comment, index) in list" :key="index" :title="comment.content" /> -->
+      <comment-item
+        v-for="(comment, index) in list"
+        :key="index"
+        :comment="comment"
+      />
     </van-list>
   </div>
 </template>
 
 <script>
-// import { getComments } from '../../../api/article'
+import { getComments } from '../../../api/article'
+import CommentItem from './comment-item'
 
 export default {
   name: 'CommentList',
+  components: {
+    CommentItem
+  },
   props: {
     source: {
       type: [String, Number, Object],
@@ -26,18 +36,33 @@ export default {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      offset: null, // 获取下一页的页码
+      limit: 10 // 每页大小
     }
   },
   methods: {
-    // async onLoad () {
-    //   const { data } = await getComments({
-    //     type: 'a',  // 评论类型 ‘a’为对文章的评论 ‘c’为对评论的回复
-    //     source: this.source, // 源id  即文章id或者评论id
-    //     offset: , // 文章偏移量 即页码 值为评论id
-    //     limit:
-    //   })
-    // }
+    async onLoad () {
+      const { data } = await getComments({
+        type: 'a', // 评论类型 ‘a’为对文章的评论 ‘c’为对评论的回复
+        source: this.source, // 源id  即文章id或者评论id
+        offset: this.offset, // 文章偏移量 即页码 值为评论id
+        limit: this.limit // 每页大小
+      })
+
+      const { results } = data.data
+      console.log(results)
+      // 将请求的结果添加到列表中
+      this.list.push(...results)
+      // 请求一页完成后 关闭loading状态
+      this.loading = false
+      // 判断剩下页码 如果还有下一页 页码赋值为下一页的页码 如果没有 显示没有了
+      if (results.length) {
+        this.offset = data.data.last_id
+      } else {
+        this.finished = true
+      }
+    }
   }
 }
 </script>
